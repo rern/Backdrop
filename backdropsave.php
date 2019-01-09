b4c43b435c6be0d2e3c7dfca0ac05669cef42e31
rern <rernrern@gmail.com>
2019-01-09 11:53:20 +0700

    Add files via upload

--- backdropsave.php
+++ backdropsave.php
@@ -2,11 +2,8 @@
 $redis = new Redis();
 $redis->pconnect( '127.0.0.1' );
 foreach ( range( 1, 7 ) as $i ) {
-	$up.= ','.$_POST[ 'ms-up'.$i ];
-	$dn.= ','.$_POST[ 'ms-dn'.$i ];
+	$up.= ','.( $_POST[ 'ms-up'.$i ] ?: 0 );
+	$dn.= ','.( $_POST[ 'ms-dn'.$i ] ?: 0 );
+	$name.= ','.( $_POST[ 'inputname'.$i ] ?: 0 );
 }
-$up = ltrim( $up, ',' );
-$dn = ltrim( $dn, ',' );
-echo $up.' - '.$dn;
-$redis->hSet( 'backdrops', 'up', $up );
-$redis->hSet( 'backdrops', 'dn', $dn );
+$redis->hmSet( 'backdrops', array( 'up' => ltrim( $up, ',' ), 'dn' => ltrim( $dn, ',' ), 'name' => ltrim( $name, ',' ) ) );
