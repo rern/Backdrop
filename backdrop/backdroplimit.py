3bd39e903590915a4c8d87ecfb95dc89f74c8729
rern <rernrern@gmail.com>
2019-01-14 17:46:06 +0700

    Add files via upload

--- backdrop/backdroplimit.py
+++ backdrop/backdroplimit.py
@@ -6,17 +6,17 @@
 
 from backdropgpio import *
 
-def limitChange( pin, i ):
+def limitON( i ):
 	GPIO.output( pinDNlist[ i ], OFF )
 	
 	data = { 'updn': 'dn', 'num': i + 1 }
 	req = urllib2.Request( url, json.dumps( data ), headers = headerdata )
 	response = urllib2.urlopen( req )
-	# bash: curl -s -v -X POST 'http://localhost/pub?id=backdrop' -d '{ "updn": "dn", "num": 7, "active": 1 }'
+	# bash: curl -s -v -X POST 'http://localhost/pub?id=backdrop' -d '{ "updn": "dn", "num": 7 }'
 	
 for i in range( 0, 7 ):
 	pin = pinLimitlist[ i ]
-	GPIO.add_event_detect( pin, GPIO.FALLING, callback = lambda channel: limitChange( pin, i ), bouncetime = 300 )
+	GPIO.add_event_detect( pin, GPIO.FALLING, callback = lambda channel: limitON( i ), bouncetime = 300 )
 
 while True:
 	time.sleep( 10 )
